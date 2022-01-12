76f1aa1b5bf1b64ac31ed6cf60f5738afbb43974
zzjing <65733145+zzjing@users.noreply.github.com>
2022-01-12 09:48:57 -0800

    Update IndentationCheck.java

--- id/IndentationCheck.java
+++ id/IndentationCheck.java
@@ -2,20 +2,20 @@ public class Python_Indentation_Validation {
     public boolean validate(String[] lines) {
         Stack<String> codeBuffer = new Stack<>();
         for (String line : lines) {
-            int blockLevel = getIndentNum(line);
-            // check first line, stack is initially empty
+            int blockLevel = getIndentNum(line); // every step needs to check how many indentation
+            // 1. check first line, stack is initially empty
             if (codeBuffer.isEmpty()) {
                 if (blockLevel != 0) { // first line, must be 0 indents
                     // print out line
                     return false;
                 }
-            } else if (codeBuffer.peek().charAt(codeBuffer.peek().length() - 1) == ':') { // ends with control :
+            } else if (codeBuffer.peek().charAt(codeBuffer.peek().length() - 1) == ':') { // 2. ends with control :
                 if (blockLevel != getIndentNum(codeBuffer.peek()) + 1) { // must be one indent more than last line's
                     // print out line
                     return false;
                 }
             } else {
-                // if one "nested" statement block finishes, pop all nested lines with more indents
+                // 3. if one "nested" statement block finishes, pop all nested lines with more indents
                 while (!codeBuffer.isEmpty() && getIndentNum(codeBuffer.peek()) > blockLevel) {
                     codeBuffer.pop();
                 }
@@ -43,3 +43,4 @@ public class Python_Indentation_Validation {
         return indents;
     }
 }
+// Time O(n^2) Space(n)
